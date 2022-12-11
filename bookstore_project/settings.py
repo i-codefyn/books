bbda1c062f8b321910dc34ffc5e51ea44dd19027
codefyn <manis.shr@gmail.com>
2022-12-11 19:45:23 +0530

    ch6

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -39,7 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig', # new
     'users.apps.UsersConfig', # new
+    # Third-party
+    'crispy_forms', # new
 ]
+# django-crispy-forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4' # new
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -55,6 +59,14 @@ ROOT_URLCONF = 'bookstore_project.urls'
 
 LOGIN_REDIRECT_URL = 'home'  #NEW
 LOGOUT_REDIRECT_URL = 'home' # new
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_FINDERS = [
+"django.contrib.staticfiles.finders.FileSystemFinder",
+"django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
 
 
 TEMPLATES = [
@@ -124,7 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
