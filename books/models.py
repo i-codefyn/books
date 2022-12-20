8399f240669c5a021ddbe4c60c3ad3566f9c2181
codefyn <manis.shr@gmail.com>
2022-12-20 18:24:07 +0530

    ch13

--- books/models.py
+++ books/models.py
@@ -5,21 +5,26 @@ from django.urls import reverse
 
 
 class Book(models.Model):
-   id = models.UUIDField( # new
+   id = models.UUIDField(
      primary_key=True,
      default=uuid.uuid4,
      editable=False)
-
    title = models.CharField(max_length=200)
    author = models.CharField(max_length=200)
    price = models.DecimalField(max_digits=6, decimal_places=2)
-   cover = models.ImageField(upload_to='covers/', blank=True) # new
+   cover = models.ImageField(upload_to='covers/', blank=True)
+
+   class Meta: # new
+     permissions = [
+('special_status', 'Can read all books'),
+]
+
 
    def __str__(self):
-      return self.title
+       return self.title
 
-   def get_absolute_url(self): # new
-      return reverse('book_detail', kwargs={'pk': str(self.pk)})
+   def get_absolute_url(self):
+       return reverse('book_detail', args=[str(self.id)])
 
 
 class Review(models.Model): # new
--- books/views.py
+++ books/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import (
+LoginRequiredMixin,
+PermissionRequiredMixin # new
+)
 from django.views.generic import ListView, DetailView
 from .models import Book
 
 
 
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin,  ListView): # new
    model = Book
    context_object_name = 'book_list' # new
    template_name = 'books/book_list.html'
+   login_url = 'account_login' # new
 
 
-class BookDetailView(DetailView): # new
+class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin,DetailView): # new
+
    model = Book
    context_object_name = 'book' # new
    template_name = 'books/book_detail.html'
+   login_url = 'account_login' # new
+   permission_required = 'books.special_status' # new
